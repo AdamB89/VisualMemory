269efe3c903080efce464ddc79ff9d6c518097ce
Adam Baram <adam_baram@windowslive.com>
2018-09-01 12:41:28 +0300

    updated image path

--- src/app/shared/Items.ts
+++ src/app/shared/Items.ts
@@ -5,22 +5,22 @@ export const ITEMS:Item[]=[
     {
         id: 0,
         name: 'london',
-        image: '/assets/images/london.jpg'
+        image: './assets/images/london.jpg'
     },
     {
         id: 1,
         name: 'paris',
-        image: '/assets/images/paris.jpg'
+        image: './assets/images/paris.jpg'
     },
     {
         id: 2,
         name: 'madrid',
-        image: '/assets/images/madrid.jpg'
+        image: './assets/images/madrid.jpg'
     },
     {
         id: 3,
         name: 'vienna',
-        image: '/assets/images/vienna.jpg'
+        image: './assets/images/vienna.jpg'
     },
 
 
